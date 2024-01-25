b2a4d7558a9f5a1b1d376100e233c1034a2857e9
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-25 13:46:58 +0100

    import test works

--- cli/import.js
+++ cli/import.js
@@ -1,9 +1,20 @@
 import { addSkill } from "../src/handlers.js";
 import * as tools from './tools.js';
 
+const locked = true;
+
 const _skills = tools.readJsonFile('cli/data/skills.json');
-console.log(_skills);
 
-
-// const skill = await addSkill(_skill);
-// console.log('added', skill);
+for (const _skill of _skills) {
+	try {
+		if (locked) {
+			console.log(`TEST IMPORT OF "${_skill.name}"`);
+		} else {
+			const skill = await addSkill(_skill);
+			console.log(`imported skills ${_skill.name}`);
+		}
+	}
+	catch (e) {
+		console.log(`ERROR ${e.message}`);
+	}
+}
