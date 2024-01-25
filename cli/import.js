9ac2a98574f3f446b2b76f775bc64b49af081c08
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-25 13:34:42 +0100

    imports mock skill

--- cli/import.js
+++ cli/import.js
@@ -1,1 +1,11 @@
-console.log('in the cli');
+import { addSkill } from "../src/handlers.js";
+
+const _skill = {
+	"idCode": "777",
+	"name": "nnn",
+	"url": "uuu",
+	"description": "ddd"
+};
+
+const skill = await addSkill(_skill);
+console.log('added', skill);
